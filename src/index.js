5da4d9793e525c2e7e8a16b871b1756745fb0283
RiveroMadera <jesusrivero19@gmail.com>
2021-06-10 14:20:53 -0400

    more messages, change readme.md

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const Messages = [
   "Que la fuerza te acompañe!",
-  "Hakuna Matata",
+  "¡Hakuna Matata!",
   "C'est la Vie",
   "I'm not a robot",
   "This is the way!",
   "Que el producto resultante de la masa por la aceleracion te acompañe",
   "Hello World",
-  "si vis pasem parabellum!"
+  "Before software can be reusable it first has to be usable. (Ralph Johnson)", 
+  "It’s not a bug – it’s an undocumented feature. (Anonymous)",
+  "The best thing about a boolean is even if you are wrong, you are only off by a bit. (Anonymous)",
+  "Deleted code is debugged code. (Jeff Sickel)",
+  "Don’t worry if it doesn’t work right. If everything did, you’d be out of a job. (Mosher’s Law of Software Engineering)",
+  //"Keyboard not found. Press a button on the non-existent keyboard to fix it. ° _ ° "
+
 ];
 
 const randomMsg = () => {
